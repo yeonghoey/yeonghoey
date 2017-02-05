d50f868dda5f8dc42e4406ee53221249ac251125
yeonghoey <yeonghoey@gmail.com>
2017-02-05 13:01:24 +0900

    Write parser
    Former-commit-id: a7457b963c3f10938c76cca59a4dc61dbc2ca2b4

--- design-of-computer-programs/parser.py
+++ design-of-computer-programs/parser.py
@@ -16,8 +16,41 @@ def split(s, pattern):
     return [p.strip() for p in re.split(pattern, s.strip())]
 
 
-def parse(start, g):
-    pass
+def parse(start, text, g):
+    def parse_atom(pattern, text):
+        pattern = r'^\s*' + '(%s)' % pattern
+        mo = re.match(pattern, text)
+        return ((mo.group(1), text[mo.end():]) if mo is not None else Fail)
+
+    def parse_sequence(sequence, text):
+        tree, remains = [], text
+        for target in sequence:
+            if target in g:
+                subtrees, r = go(target, remains)
+                if subtrees is None:
+                    return Fail
+                else:
+                    tree.append(subtrees)
+                    remains = r
+            else:
+                parsed, r = parse_atom(target, remains)
+                if parsed is None:
+                    return Fail
+                else:
+                    tree.append(parsed)
+                    remains = r
+
+        return (tree, remains)
+
+    def go(target, text):
+        sequences = g[target]
+        for seq in sequences:
+            subtrees, remains = parse_sequence(seq, text)
+            if subtrees is not None:
+                return ([target] + subtrees, remains)
+        return Fail
+
+    return go(start, text)
 
 
 G = grammer('''
@@ -27,4 +60,7 @@ Factor => Num              | [(] Expr [)]
 Num    => [0-9]+
 ''')
 
+Fail = (None, None)
+
 pprint(G)
+pprint(parse('Expr', '4 * (1 + 2) * 3', G))
