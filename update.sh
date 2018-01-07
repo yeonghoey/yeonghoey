ebdc18f77f8c2db69092241f6f99c7dca39cac40
Yeongho Kim <yeonghoey@yeonghoey-air.local>
2018-01-07 16:00:04 +0900

    Init git lfs when update.sh

--- update.sh
+++ update.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 readonly REPO="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Ensure lfs initialized
+git lfs install --local
+
 # NOTE: Smudging is much slower than 'git lfs pull'
 GIT_LFS_SKIP_SMUDGE=1 \
   git -C "$REPO" pull --rebase --autostash
