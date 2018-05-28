9a9515274085438674ce718ea2aee404fe833b04
yeonghoey <yeonghoey@gmail.com>
2018-05-29 01:28:17 +0900

    Update filter

--- scripts/filter.py
+++ scripts/filter.py
@@ -3,7 +3,7 @@ from os.path import dirname
 from pprint import pprint
 from sys import stderr
 
-from pandocfilters import Image, toJSONFilters
+import pandocfilters as pf
 
 
 SRC = environ['YHY_FILTER_SRC']
@@ -13,14 +13,33 @@ DEBUG = environ.get('YHY_FILTER_DEBUG') is not None
 CONTEXT_PATH = dirname(SRC)
 
 
+# When `handle_<function>` returns:
+# pf.Foo : replace (spans as spans, divs as divs only)
+#   None : do nohting
+#     [] : delete
+
+def handle_notag(key, value, format, meta):
+    if key == 'Span':
+        attr, inlines = value
+        _, classes, _ = attr
+        if 'tag' in classes:
+            return []
+
+
 def handle_basepath(key, value, format, meta):
     if key == 'Image':
-        value[2][0] = basepath(value[2][0])
-        return Image(*value)
+        attr, inlines, target = value
+        url, title = target
+        return pf.Image(attr, inlines, (basepath(url), title))
 
 
-def handle_debug(*arg):
-    pprint(arg, stream=stderr)
+def handle_debug(*args):
+    print(file=stderr)
+    names = 'key value format meta'.split()
+    for n, a in zip(names, args):
+        print(f'{n:6}: ', end='', file=stderr)
+        pprint(a, indent=4, stream=stderr)
+    print(file=stderr)
 
 
 def basepath(url, raw=True):
@@ -29,7 +48,8 @@ def basepath(url, raw=True):
 
 
 if __name__ == '__main__':
-    toJSONFilters(filter(None, [
-        handle_basepath if BASE else None,
+    pf.toJSONFilters(filter(None, [
         handle_debug if DEBUG else None,
+        handle_notag,
+        handle_basepath if BASE else None,
     ]))
