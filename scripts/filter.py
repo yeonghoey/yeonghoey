08d68408236a7cd7f05f582203af12d844bf6433
Yeongho Kim <yeonghoey@gmail.com>
2018-05-31 13:13:30 +0900

    Add slides support

--- scripts/filter.py
+++ scripts/filter.py
@@ -1,36 +1,37 @@
 from os import environ
 from os.path import dirname
 from pprint import pprint
+import re
 from sys import stderr
+from textwrap import dedent
 
 import pandocfilters as pf
 
 
-SRC = environ['YHY_FILTER_SRC']
-BASE = environ['YHY_FILTER_BASE']
 DEBUG = environ.get('YHY_FILTER_DEBUG') is not None
 
+# =============================================================================
+
+
+SRC = environ['YHY_FILTER_SRC']
+BASE = environ['YHY_FILTER_BASE']
 CONTEXT_PATH = dirname(SRC)
 
 
+def basepath(url, raw=True):
+    if BASE:
+        type_ = 'raw' if raw else 'blob'
+        return '/'.join([BASE, type_, 'master', CONTEXT_PATH, url])
+    else:
+        return url
+
+
+# =============================================================================
 # When `handle_<function>` returns:
 # pf.Foo : replace (spans as spans, divs as divs only)
 #   None : do nohting
 #     [] : delete
-
-def handle_notag(key, value, format, meta):
-    if key == 'Span':
-        attr, inlines = value
-        _, classes, _ = attr
-        if 'tag' in classes:
-            return []
-
-
-def handle_basepath(key, value, format, meta):
-    if key == 'Image':
-        attr, inlines, target = value
-        url, title = target
-        return pf.Image(attr, inlines, (basepath(url), title))
+# =============================================================================
 
 
 def handle_debug(*args):
@@ -42,14 +43,61 @@ def handle_debug(*args):
     print(file=stderr)
 
 
-def basepath(url, raw=True):
-    type_ = 'raw' if raw else 'tree'
-    return '/'.join([BASE, type_, 'master', CONTEXT_PATH, url])
+# =============================================================================
+
+
+def handle_image(key, value, format, meta):
+    if key == 'Image':
+        attr, inlines, target = value
+        url, title = target
+        return pf.Image(attr, inlines, (basepath(url), title))
+
+
+# =============================================================================
+
+
+def handle_notag(key, value, format, meta):
+    if key == 'Span':
+        attr, inlines = value
+        _, classes, _ = attr
+        if 'tag' in classes:
+            return []
+
+
+# =============================================================================
+# YEONGHOEY_PDF[<width>x<height>] <src>
+YEONGHOEY_PDF = re.compile(r'''^YEONGHOEY_PDF
+                               (?:\[(?P<width>[0-9%]+)x(?P<height>[0-9%]+)\])?
+                               [ ]
+                               (?P<src>.+)$''', re.VERBOSE)
+
+
+def handle_pdf(key, value, format, meta):
+    assert format == 'html'
+    if key == 'CodeBlock':
+        attr, code = value
+
+        match = YEONGHOEY_PDF.match(code)
+        if match is not None:
+            d = match.groupdict(default='100%')
+            width = d['width']
+            height = d['height']
+            src = basepath(d['src'])
+            return pf.RawBlock(format, dedent(f'''\
+            <embed src="{src}"
+                   type="application/pdf"
+                   width="{width}"
+                   height="{height}">
+            '''))
+
+
+# =============================================================================
 
 
 if __name__ == '__main__':
     pf.toJSONFilters(filter(None, [
         handle_debug if DEBUG else None,
+        handle_image,
         handle_notag,
-        handle_basepath if BASE else None,
+        handle_pdf,
     ]))
